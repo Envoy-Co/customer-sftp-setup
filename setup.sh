f7b0467b8d91c1d6e3966151a1ad2fda13866f07
Eric Dahl <eric@skeyelab.com>
2020-05-13 17:07:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+clear
 
 cd ~/.ssh
-ssh-keygen -P "" -f envoy-sftp-key &
-echo cat ~/.ssh/envoy-sftp-key.pub
+ssh-keygen -P "" -f envoy-sftp-key >/dev/null 2>&1
+echo "Envoy.co Customer SFTP Configuation Tool"
+echo ""
+echo "Please copy everything in this section and email it to: sftpacccess@goenvoy.co"
+echo "------------------------------------------------"
+echo ""
+cat ~/.ssh/envoy-sftp-key.pub
+echo ""
+echo "------------------------------------------------"
